9f946cfbb7734c61b7346e839e305103cddde047
Volk <dylan.volk@accenturefederal.com>
2018-12-27 22:30:06 -0500

    Add functionality to move mouse to keep a session alive

--- MouseJiggler.py
+++ MouseJiggler.py
@@ -1,1 +1,13 @@
 ### Mouse Jiggler ###
+# pip install pyautogui
+
+import pyautogui
+print(pyautogui.size())
+
+go=1
+while go>0:
+    pyautogui.dragRel(100, 0, duration = 1) 
+    pyautogui.dragRel(0, 100, duration = 1) 
+    pyautogui.dragRel(-100, 0, duration = 1) 
+    pyautogui.dragRel(0, -100, duration = 1)
+#press ctl + alt + M to stop code from running
