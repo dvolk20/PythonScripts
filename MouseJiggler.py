0d06165005a9be5263767e70db9bb925b30ab694
Volk <dylan.volk@accenturefederal.com>
2019-01-14 16:53:31 -0500

    update comments

--- MouseJiggler.py
+++ MouseJiggler.py
@@ -10,4 +10,6 @@ while go>0:
     pyautogui.dragRel(0, 100, duration = 1) 
     pyautogui.dragRel(-100, 0, duration = 1) 
     pyautogui.dragRel(0, -100, duration = 1)
-#press ctl + alt + M to stop code from running
+
+#press ctl + alt + N to START
+#press ctl + alt + M to STOP 
